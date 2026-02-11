bfedbbb6b449905779b555a294c55a48cf8c7f3e
ankelkoss <70823492+ankelkoss@users.noreply.github.com>
2026-02-11 23:47:34 +0200

    Create CreateOrderLineDto.cs

---
+++ InternetShop/Dtos/OrderLine/CreateOrderLineDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InternetShop.Dtos.OrderLine
+{
+    public class CreateOrderLineDto
+    {
+        [Range(1, int.MaxValue)]
+        public int ProductId { get; set; }
+
+        [Range(1, 999999)]
+        public int Quantity { get; set; }
+    }
+}
