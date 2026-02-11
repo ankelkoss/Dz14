3d46248f4c27d9cf60c77de49a9196d4ebb51f71
ankelkoss <70823492+ankelkoss@users.noreply.github.com>
2026-02-11 23:13:22 +0200

    Create ShopDbContext.cs

---
+++ InternetShop/Data/ShopDbContext.cs
@@ -0,0 +1,26 @@
+﻿using InternetShop.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace InternetShop.Data
+{
+    public class ShopDbContext : DbContext
+    {
+        public ShopDbContext(DbContextOptions<ShopDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderLine> OrderLines { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ShopDbContext).Assembly);
+
+            //new DbInitializer(modelBuilder).Seed();
+        }
+    }
+}
