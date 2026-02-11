19281fb6283a1837068b364dc165c3e03afba37f
ankelkoss <70823492+ankelkoss@users.noreply.github.com>
2026-02-11 23:13:14 +0200

    Create OrderLineConfiguration.cs

---
+++ InternetShop/Data/EntityConfigurations/OrderLineConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using InternetShop.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection.Emit;
+
+namespace InternetShop.Data.EntityConfigurations
+{
+    public class OrderLineConfiguration : IEntityTypeConfiguration<OrderLine>
+    {
+        public void Configure(EntityTypeBuilder<OrderLine> builder)
+        {
+
+            builder.Property(ol => ol.UnitPrice)
+                .HasPrecision(18, 2);
+        }
+    }
+}
