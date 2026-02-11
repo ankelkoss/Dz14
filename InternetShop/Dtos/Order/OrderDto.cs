5736d7100cb63c4b78b833a35cafbecf11f62070
ankelkoss <70823492+ankelkoss@users.noreply.github.com>
2026-02-11 23:47:31 +0200

    Create OrderDto.cs

---
+++ InternetShop/Dtos/Order/OrderDto.cs
@@ -0,0 +1,7 @@
+﻿using InternetShop.Dtos.OrderLine;
+using InternetShop.Models;
+
+namespace InternetShop.Dtos.Order
+{
+    public record OrderDto(int Id, int CustomerId, string? CustomerEmail, DateTime CreatedAtUtc, OrderStatus Status, List<OrderLineDto> Lines);
+}
