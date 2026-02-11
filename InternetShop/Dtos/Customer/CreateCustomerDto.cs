4097630166633e9319bd54d613ba89aae8485853
ankelkoss <70823492+ankelkoss@users.noreply.github.com>
2026-02-11 23:47:07 +0200

    Create CreateCustomerDto.cs

---
+++ InternetShop/Dtos/Customer/CreateCustomerDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InternetShop.Dtos.Customer
+{
+    public class CreateCustomerDto
+    {
+        [Required, MaxLength(120)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required, MaxLength(120)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required, EmailAddress, MaxLength(254)]
+        public string Email { get; set; } = string.Empty;
+
+        [MaxLength(30)]
+        public string? Phone { get; set; }
+    }
+}
