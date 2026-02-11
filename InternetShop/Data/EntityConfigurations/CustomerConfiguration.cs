a30880b597af27d5b6b2f206b6420be1d1499618
ankelkoss <70823492+ankelkoss@users.noreply.github.com>
2026-02-11 23:13:07 +0200

    Create CustomerConfiguration.cs

---
+++ InternetShop/Data/EntityConfigurations/CustomerConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using InternetShop.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection.Emit;
+
+namespace InternetShop.Data.EntityConfigurations
+{
+    public class ConsumerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            // Уникальность Email клиента
+            builder.HasIndex(c => c.Email)
+                .IsUnique();
+        }
+    }
+}
